b304ca386989d8b0f00963adaf694559e283849e
Patrick Koldborg <akoldborg@gmail.com>
2021-09-02 12:13:12 +0200

    Removed newline that accidentally made it into 6f1a35

--- Testing/Test.Base/Data/MemberData.cs
+++ Testing/Test.Base/Data/MemberData.cs
@@ -4,7 +4,6 @@ using System.Linq;
 
 namespace Test.TestBase
 {
-    
     /// <summary>
     /// Contains all data members that will be used for inline data in the testing methods.
     /// </summary>
