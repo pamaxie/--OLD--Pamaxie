3fee914098ccd1a4a6f6c253f08d2187a5d1026b
LeechFilcher <19296864-github@lukasduerr.de>
2021-12-10 14:19:07 +0100

    Breaking Changes, Fixing Project Sturctures to avoid big projects

--- Database/Pamaxie.Database.Redis/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Test.Base")]
-[assembly: InternalsVisibleTo("Test.Pamaxie.Database.Extensions.Client")]
-[assembly: InternalsVisibleTo("Test.Pamaxie.Database.Extensions.Server")]
-[assembly: InternalsVisibleTo("Test.Pamaxie.Database.Api")]
